d6f9378b02c36bcd7acdc9a9c1ad9dcedecdac64
JackForr <jackforrester09@gmail.com>
2022-07-29 21:49:15 +0100

    task 1 initial

---
+++ Task_1/data_cleaning_script/cleaning_script.R
@@ -0,0 +1,22 @@
+library(readr)
+library(tidyverse)
+library(tidyr)
+decathlon_data <- read_rds("raw_data/decathlon.rds")
+view(decathlon_data)
+glimpse(decathlon_data)
+
+#removing row names & cleaning names
+clean_decathlon_data <- decathlon_data %>% 
+  rownames_to_column(var = "name") %>% 
+  janitor::clean_names() %>% 
+  mutate_if(is.character, str_to_lower)
+
+#long formatting
+long_decathlon_data <- clean_decathlon_data %>% 
+  pivot_longer(-c(name, points, competition ),
+names_to = "discipline",
+values_to = "score")
+
+view(long_decathlon_data)
+
+write.csv(long_decathlon_data, file = "long_decathlon_data.csv")
