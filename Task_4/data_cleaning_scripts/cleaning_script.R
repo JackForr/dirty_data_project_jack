fd846f7b4de8742ba595db89de39c6586b6f42cb
JackForr <jackforrester09@gmail.com>
2022-08-01 21:34:26 +0100

    hard coding for america

--- Task_4/data_cleaning_scripts/cleaning_script.R
+++ Task_4/data_cleaning_scripts/cleaning_script.R
@@ -110,14 +110,35 @@ full_data <- full_data %>%
          reaction = na_if(reaction, "Friday"),
          reaction = na_if(reaction, "Sunday"))
 
-full_data %>% 
-str_detect("[aA]merica")
+#cleaning country column
+
+usa_outliers = c("Alaska", "California", "EUA", "Merica", "Murica", "murrika",
+                 "New Jersey", "New York", "North Carolina", "Pittsburgh", 
+                 "The Yoo Ess of Aaayyyyyy", "Trumpistan", "U S", "u s a", "u.s.",
+                 "U.s.", "U.S.", "u.s.a.", "U.S.A.", "UD", "us", "Us", "US", 
+                 "US of A", "USSA", "'merica")
+
+silly_values = c("A tropical island south of the equator", "A", "Atlantis",
+                 "Canae", "cascadia ", "Cascadia", "Denial", "Earth", 
+                 "Fear and Loathing", "god's country", "I don't know anymore",
+                 "insanity lately", "there isn't one for old men", 
+                 "soviet canuckistan", "Narnia", "Neverland",
+                 "one of the best ones", "See above", "Somewhere", 
+                 "subscribe to dm4uz3 on youtube",
+                 "The republic of Cascadia", "this one")
+
+US_deviations <- c("usa", "USA", "United States of America", "uSA", "united states",  "United States", "USA!", "USA (I think but it's an election year so who can really tell)", "Usa" , "America", "Units States", "United states", "USA USA USA", "the best one - usa" , "USA! USA! USA!", "united states of america", "USA!!!!!!", "USA! USA!", "United Sates", "Sub-Canadian North America... 'Merica", "UNited States", "United Stetes", "america", "Not the USA or Canada", "USA USA USA USA", "United  States of America" , "United State", "United staes", "USAUSAUSA", "The United States", "The United States of America", "USA? Hard to tell anymore..", "USa", "I pretend to be from Canada, but I am really from the United States.", "Ahem....Amerca", "United Stated", "United Statss", "united States", "N. America", "United Statea", "USA USA USA!!!!")                                                           
+
+full_data <- full_data %>%
+  mutate(country = if_else(country %in% silly_values, NA_character_, country),
+         country = if_else(country %in% usa_outliers, "US", country),
+         country = if_else(country %in% US_deviations, "US", country))
 
 unique(full_data$country)
 
-
-
 write.csv(full_data, file = "full_data.csv")
+
+
 
   
 
