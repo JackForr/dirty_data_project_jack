855b4234cc82839b43ae7fc8b9a2c9fe55fbf778
JackForr <jackforrester09@gmail.com>
2022-08-02 13:12:33 +0100

    restructured cleaning

--- Task_4/data_cleaning_scripts/cleaning_script.R
+++ Task_4/data_cleaning_scripts/cleaning_script.R
@@ -1,3 +1,4 @@
+### read in data 
 library(readxl)
 library(tidyverse)
 library(tidyr)
@@ -8,12 +9,11 @@ halloween_candy_2016 <- read_excel("raw_data/boing-boing-candy-2016.xlsx") %>%
 halloween_candy_2017 <- read_excel("raw_data/boing-boing-candy-2017.xlsx") %>% 
   janitor::clean_names()
 
-#checking column types
-names(halloween_candy_2016)
-names(halloween_candy_2015)
-names(halloween_candy_2017)
+glimpse(halloween_candy_2015)
+glimpse(halloween_candy_2016)
+glimpse(halloween_candy_2017)
 
-#removing unneccesary columns
+### remove unneccesary columns
 halloween_candy_2016 <- halloween_candy_2016 %>% select(-c(107:123))
 
 halloween_candy_2015 <- halloween_candy_2015 %>%
@@ -22,9 +22,7 @@ halloween_candy_2015 <- halloween_candy_2015 %>%
 halloween_candy_2017 <- halloween_candy_2017 %>% 
   select(-c(110:120))
 
-
-#pivoting 2015&16
-
+###pivot
 long_2015 <- halloween_candy_2015 %>% 
   pivot_longer(-c(timestamp, how_old_are_you, are_you_going_actually_going_trick_or_treating_yourself),
                names_to = "candy_type",
@@ -35,36 +33,21 @@ long_2016 <- halloween_candy_2016 %>%
                names_to = "candy_type",
                values_to = "reaction")
 
-
-
-#removing q prefix 
-halloween_candy_2017 <- halloween_candy_2017 %>% 
+halloween_candy_2017 <- halloween_candy_2017 %>% #removing prefix to col names
   rename_all(~stringr::str_replace(., "^q[0-9]_", "")) %>% 
   rename_all(~stringr::str_replace(., "^q[0-9]{2}_", ""))
-  
 
-#pivoting 2017
 long_2017 <- halloween_candy_2017 %>% 
   pivot_longer(-c(internal_id, going_out, gender, age, country, state_province_county_etc),
                names_to = "candy_type",
                values_to = "reaction")
 
-#removing missing reactions
-long_2015 <- long_2015 %>% 
-  drop_na(reaction)
-
-long_2016 <- long_2016 %>% 
-  drop_na(reaction)
-
-long_2017 <- long_2017 %>% 
-  drop_na(reaction)
-
-#renaming columns & matching types
+### matching column names and types
 long_2017 <- long_2017 %>% 
   rename("state_or_province" = state_province_county_etc,
          "year" = internal_id) %>% 
   mutate(`year` = as.numeric(`year`),
-         `age` = as.integer(`age`))
+         `age` = as.integer(`age`)) #coercing NAs to non-integer entries
 
 long_2016 <- long_2016 %>% 
   rename("going_out" = are_you_going_actually_going_trick_or_treating_yourself,
@@ -72,37 +55,27 @@ long_2016 <- long_2016 %>%
          "age" = how_old_are_you,
          "country" = which_country_do_you_live_in,
          "state_or_province" = which_state_province_county_do_you_live_in,
-         "year" = timestamp
-         ) %>% 
+         "year" = timestamp) %>% 
   mutate(`year` = as.numeric(`year`),
-         `age` = as.integer(`age`))
+         `age` = as.integer(`age`)) #coercing NAs to non-integer entries
 
 long_2015 <- long_2015 %>% 
   rename("age" = how_old_are_you,
          "going_out" = are_you_going_actually_going_trick_or_treating_yourself,
-         "year" = timestamp
-         ) %>% 
+         "year" = timestamp) %>% 
   mutate(`year` = as.numeric(`year`),
-         `age` = as.integer(`age`))
+         `age` = as.integer(`age`)) #coercing NAs to non-integer entries
 
-#recode to define year of observation
-
+### defining year of observation
 long_2017$year <- 2017
-
 long_2016$year <- 2016
-
 long_2015$year <- 2015
 
-
-#joining datasets
- 
+### join
 full_data <- full_join(long_2016, long_2015) %>% 
-    full_join(., long_2017)
+  full_join(., long_2017)
 
-glimpse(full_data)
-
-#cleaning variables
-
+### coalesce/drop missing values
 full_data <- full_data %>% 
   mutate(going_out = coalesce(going_out, "unknown"),
          country = coalesce(country, "unknown"),
@@ -111,10 +84,10 @@ full_data <- full_data %>%
          reaction = na_if(reaction, "1.0"),
          reaction = na_if(reaction, "2.0"),
          reaction = na_if(reaction, "Friday"),
-         reaction = na_if(reaction, "Sunday"))
+         reaction = na_if(reaction, "Sunday")) %>% 
+  drop_na(reaction)
 
-#cleaning country column
-
+### cleaning observations
 usa_outliers = c("Alaska", "California", "EUA", "Merica", "Murica", "murrika",
                  "New Jersey", "New York", "North Carolina", "Pittsburgh", 
                  "The Yoo Ess of Aaayyyyyy", "Trumpistan", "U S", "u s a", "u.s.",
@@ -130,17 +103,20 @@ silly_values = c("A tropical island south of the equator", "A", "Atlantis",
                  "subscribe to dm4uz3 on youtube",
                  "The republic of Cascadia", "this one")
 
-US_deviations <- c("usa", "USA", "United States of America", "uSA", "united states",  "United States", "USA!", "USA (I think but it's an election year so who can really tell)", "Usa" , "America", "Units States", "United states", "USA USA USA", "the best one - usa" , "USA! USA! USA!", "united states of america", "USA!!!!!!", "USA! USA!", "United Sates", "Sub-Canadian North America... 'Merica", "UNited States", "United Stetes", "america", "Not the USA or Canada", "USA USA USA USA", "United  States of America" , "United State", "United staes", "USAUSAUSA", "The United States", "The United States of America", "USA? Hard to tell anymore..", "USa", "I pretend to be from Canada, but I am really from the United States.", "Ahem....Amerca", "United Stated", "United Statss", "united States", "N. America", "United Statea", "USA USA USA!!!!")                                                           
+US_deviations <- c("usa", "USA", "United States of America", "uSA", "united states",  "United States", "USA!", "USA (I think but it's an election year so who can really tell)", "Usa" , "America", "Units States", "United states", "USA USA USA", "the best one - usa" , "USA! USA! USA!", "united states of america", "USA!!!!!!", "USA! USA!", "United Sates", "Sub-Canadian North America... 'Merica", "UNited States", "United Stetes", "america", "Not the USA or Canada", "USA USA USA USA", "United  States of America" , "United State", "United staes", "USAUSAUSA", "The United States", "The United States of America", "USA? Hard to tell anymore..", "USa", "I pretend to be from Canada, but I am really from the United States.", "Ahem....Amerca", "United Stated", "United Statss", "united States", "N. America", "United Statea", "USA USA USA!!!!")   
+
+canadian_deviations <- c("canada", "Can", "Canada`", "CANADA")
+
+UK_deviations <- c("uk", "Uk", "United Kindom", "England", "United Kingdom", "endland", "Scotland", "england")
 
 full_data <- full_data %>%
   mutate(country = if_else(country %in% silly_values, NA_character_, country),
          country = if_else(country %in% usa_outliers, "US", country),
-         country = if_else(country %in% US_deviations, "US", country))
+         country = if_else(country %in% US_deviations, "US", country),
+         country = if_else(country %in% canadian_deviations, "Canada", country),
+         country = if_else(country %in% UK_deviations, "UK", country))
 
-#cleaning age column
-unique(full_data$age)
-
-
+### writing clean data 
 write.csv(full_data, file = "full_data.csv")
 
 
